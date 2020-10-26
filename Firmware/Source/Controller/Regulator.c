05453be9591158c1a39b8bd60659ed1f5e7482d2
asytyi <alexander.a.sytyi@gmail.com>
2020-10-26 12:31:43 +0300

    Доработаны функции инициализации и вызова регулятора

--- Firmware/Source/Controller/Regulator.c
+++ Firmware/Source/Controller/Regulator.c
@@ -15,40 +15,80 @@ typedef struct __RegulatorSettings
 	float Kp;
 	float Ki;
 	float Control;
+	FUNC_CallbackSetControl SetControl;
 } RegulatorSettings, *pRegulatorSettings;
 
 // Variables
 static RegulatorSettings VoltageRegulator, CurrentRegulator;
+static pRegulatorSettings ActiveRegulator;
+
+// Forward functions
+void REGULATOR_ActivateX(pRegulatorSettings ActiveRegulator, FUNC_CallbackSetControl ControlFunction,
+		uint16_t RegP, uint16_t RegI);
 
 // Functions
 //
-void REGULATOR_CycleX(pRegulatorSettings Regulator, float SampleValue, FUNC_CallbackSetControl CallbackFunc)
+void REGULATOR_Cycle(float SampleValue)
 {
 	float ControlI = 0;
-	float Error = Regulator->TargetValuePrev - SampleValue;
+	float Error = ActiveRegulator->TargetValuePrev - SampleValue;
 
 	// Расчёт интегральной ошибки
-	if(Regulator->Ki)
+	if(ActiveRegulator->Ki)
 	{
-		Regulator->ErrorI += Error;
+		ActiveRegulator->ErrorI += Error;
 
 		// Проверка насыщения
-		if(fabsf(Regulator->ErrorI) > REGLTR_ERROR_I_SAT_H)
-			Regulator->ErrorI = (Regulator->ErrorI > 0) ? REGLTR_ERROR_I_SAT_H : -REGLTR_ERROR_I_SAT_H;
+		if(fabsf(ActiveRegulator->ErrorI) > REGLTR_ERROR_I_SAT_H)
+			ActiveRegulator->ErrorI = (ActiveRegulator->ErrorI > 0) ? REGLTR_ERROR_I_SAT_H : -REGLTR_ERROR_I_SAT_H;
 
-		ControlI = Regulator->ErrorI * Regulator->Ki;
+		ControlI = ActiveRegulator->ErrorI * ActiveRegulator->Ki;
 	}
 	else
 	{
-		Regulator->ErrorI = 0;
+		ActiveRegulator->ErrorI = 0;
 		ControlI = 0;
 	}
 
 	// Корректировка управления
-	Regulator->Control = Regulator->TargetValue + Error * Regulator->Kp + ControlI;
-	Regulator->TargetValuePrev = Regulator->TargetValue;
+	ActiveRegulator->Control = ActiveRegulator->TargetValue + Error * ActiveRegulator->Kp + ControlI;
+	ActiveRegulator->TargetValuePrev = ActiveRegulator->TargetValue;
 
 	// Применение значения
-	CallbackFunc(Regulator->Control);
+	ActiveRegulator->SetControl(ActiveRegulator->Control);
+}
+// ----------------------------------------
+
+void REGULATOR_ActivateX(pRegulatorSettings ActiveRegulator, FUNC_CallbackSetControl ControlFunction,
+		uint16_t RegP, uint16_t RegI)
+{
+	ActiveRegulator->Kp = (float)DataTable[RegP] / 1000;
+	ActiveRegulator->Ki = (float)DataTable[RegI] / 1000;
+	ActiveRegulator->SetControl = ControlFunction;
+
+	ActiveRegulator->Control = 0;
+	ActiveRegulator->ErrorI = 0;
+	ActiveRegulator->TargetValue = 0;
+	ActiveRegulator->TargetValuePrev = 0;
+}
+// ----------------------------------------
+
+void REGULATOR_ActivateVoltage(FUNC_CallbackSetControl ControlFunction)
+{
+	ActiveRegulator = &VoltageRegulator;
+	REGULATOR_ActivateX(ActiveRegulator, ControlFunction, REG_VOLTAGE_KP, REG_VOLTAGE_KI);
+}
+// ----------------------------------------
+
+void REGULATOR_ActivateCurrent(FUNC_CallbackSetControl ControlFunction)
+{
+	ActiveRegulator = &CurrentRegulator;
+	REGULATOR_ActivateX(ActiveRegulator, ControlFunction, REG_CURRENT_KP, REG_CURRENT_KI);
+}
+// ----------------------------------------
+
+void REGULATOR_UpdateTarget(float TargetValue)
+{
+	ActiveRegulator->TargetValue = TargetValue;
 }
 // ----------------------------------------
--- Firmware/Source/Controller/Regulator.h
+++ Firmware/Source/Controller/Regulator.h
@@ -9,6 +9,9 @@ typedef void (*FUNC_CallbackSetControl)(float Value);
 
 // Functions
 //
-
+void REGULATOR_Cycle(float SampleValue);
+void REGULATOR_ActivateVoltage(FUNC_CallbackSetControl ControlFunction);
+void REGULATOR_ActivateCurrent(FUNC_CallbackSetControl ControlFunction);
+void REGULATOR_UpdateTarget(float TargetValue);
 
 #endif // __REGULATOR_H
