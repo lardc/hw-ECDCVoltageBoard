84ce8584f55dd1fda8fc603c6aade3dc6a5de335
asytyi <alexander.a.sytyi@gmail.com>
2020-10-27 20:37:44 +0300

    Исправлена последовательность состояний в машине состояний

--- Firmware/Source/Controller/Controller.c
+++ Firmware/Source/Controller/Controller.c
@@ -266,7 +266,7 @@ void CONTROL_PulseControl()
 					{
 						Timeout = CONTROL_TimeCounter + TIME_TRANSIENT_DELAY;
 						VB_SetLimitVIOutputs(&Config);
-						CONTROL_SetDeviceState(DS_InProcess, SS_PulseProcess);
+						CONTROL_SetDeviceState(DS_InProcess, SS_PulseWaitOutSet);
 					}
 				}
 				break;
@@ -293,6 +293,7 @@ void CONTROL_PulseControl()
 						if(CONTROL_TimeCounter > Timeout)
 						{
 							CONTROL_StartRegulator(false);
+							LL_SetStateExtLed(false);
 							LL_ResetDACOutputs();
 							CONTROL_SetDeviceState(DS_Ready, SS_None);
 						}
